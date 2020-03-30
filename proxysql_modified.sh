c91f11ea7db0c71f3b6415e400b55f59fe0aa2be
viettelidc-oss <57147861+viettelidc-oss@users.noreply.github.com>
2020-03-30 16:25:58 +0700

    Create proxysql_modified.sh

---
+++ proxysql_modified.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+QUERY="select \"INSERT INTO mysql_users (username,password,default_hostgroup) VALUES (\", CONCAT(\"'\",User,\"'\"), \",\", CONCAT(\"'\",Password,\"'\"), \",0);\" from mysql.user WHERE Password LIKE \"*%\" order by User;"
+
+if [ ! -f "/home/trove/.guestagent.prepare.end" ]; then
+    echo "Ignore ..."
+elif [[ -f "/home/trove/.proxy.end" && -f "/home/trove/.guestagent.prepare.end" ]]; then
+    QUERY="select \"INSERT INTO mysql_users (username,password,default_hostgroup) VALUES (\", CONCAT(\"'\",User,\"'\"), \",\", CONCAT(\"'\",Password,\"'\"), \",0);\" from mysql.user WHERE Password LIKE \"*%\" order by User;"
+
+    mysql -uadmin -padmin -P 6032 -e "delete from mysql_users;"
+    mysql -B -s -P 9999 -e "$QUERY" | while read line ; do mysql -uadmin -padmin -P 6032 -e "$line"; done
+    mysql -uadmin -padmin -P 6032 -e "LOAD MYSQL USERS TO RUNTIME;"
+    #mysql -P 6032 -e "SAVE MYSQL USERS TO DISK" -- No need to save to disk since we sync every 5s
+elif [[ ! -f "/home/trove/.proxy.end" && -f "/home/trove/.guestagent.prepare.end" ]]; then
+    /bin/bash /etc/proxysql_setup.sh
+else
+    echo "Ignore ...."
+fi
