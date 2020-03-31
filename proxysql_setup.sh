69cdfe000f43de2f62611f687776f8528cba5add
viettelidc-oss <57147861+viettelidc-oss@users.noreply.github.com>
2020-03-31 14:41:47 +0700

    Update proxysql_setup.sh

--- proxysql_setup.sh
+++ proxysql_setup.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-#wget -P /etc/ https://github.com/viettelidc-oss/mysqld_exporter/raw/master/proxysql_modified.deb
-#chmod 777 /etc/proxysql_modified.deb
-dpkg -i /etc/proxysql_modified.deb
+while [ ! -f "/home/trove/.guestagent.prepare.end" ]; do
 
-#Restart to change port
-service proxysql restart
+sleep 60
 
-#Need to change MariaDB / MySQL server's port to 9999
-#Add backend SQL server
-mysql -uadmin -padmin -P 6032 -e "INSERT INTO mysql_servers(hostgroup_id,hostname,port) VALUES (0,'127.0.0.1',9999);"
-mysql -uadmin -padmin -P 6032 -e "LOAD MYSQL SERVERS TO RUN;"
-mysql -uadmin -padmin -P 6032 -e "SAVE MYSQL SERVERS TO DISK;"
+done
 
-#Insert DBFirewall rule
-mysql -uadmin -padmin -P 6032 -e "INSERT INTO mysql_query_rules (rule_id,active,username,match_digest,apply) VALUES (100,1,'os_admin','.',1);"
-mysql -uadmin -padmin -P 6032 -e "INSERT INTO mysql_query_rules (rule_id,active,username,schemaname,match_digest,apply) VALUES (200,1,'os_admin','mysql','.',1);"
-mysql -uadmin -padmin -P 6032 -e "INSERT INTO mysql_query_rules (rule_id,active,match_digest,error_msg,apply) VALUES (1000,1,'^[(select)|(insert)|(update)|(delete)|(create)|(drop)|(reload)|(process)|(references)|(index)|(alter)|(show databases)|(create temporary tables)|(lock tables)|(execute)|(replication slave)|(replication client)|(create view)|(show view)|(create routine)|(alter routine)|(create user)|(event)|(trigger)].* mysql\..*','Use of system database is forbidden !',1);"
-mysql -uadmin -padmin -P 6032 -e "INSERT INTO mysql_query_rules (rule_id,active,schemaname,match_digest,error_msg,apply) VALUES (2000,1,'mysql','.','Use of system database is forbidden !',1);"
-mysql -uadmin -padmin -P 6032 -e "LOAD MYSQL QUERY RULES TO RUN;"
-mysql -uadmin -padmin -P 6032 -e "SAVE MYSQL QUERY RULES TO DISK;"
+sed '10s/3306/9999/' /etc/mysql/my.cnf >> /home/trove/my_bak.cnf && sudo mv /home/trove/my_bak.cnf /etc/mysql/my.cnf
+sudo systemctl restart mariadb.service
+
+sudo dpkg -i /etc/proxysql_modified.deb
+
+sudo service proxysql restart
+sleep 60
+
+sudo mysql -uadmin -padmin -P 6032 -e "INSERT INTO mysql_servers(hostgroup_id,hostname,port) VALUES (0,'127.0.0.1',9999);"
+sudo mysql -uadmin -padmin -P 6032 -e "LOAD MYSQL SERVERS TO RUN;"
+sudo mysql -uadmin -padmin -P 6032 -e "SAVE MYSQL SERVERS TO DISK;"
+sudo mysql -uadmin -padmin -P 6032 -e "INSERT INTO mysql_query_rules (rule_id,active,username,match_digest,apply) VALUES (100,1,'os_admin','.',1);"
+sudo mysql -uadmin -padmin -P 6032 -e "INSERT INTO mysql_query_rules (rule_id,active,username,schemaname,match_digest,apply) VALUES (200,1,'os_admin','mysql','.',1);"
+sudo mysql -uadmin -padmin -P 6032 -e "INSERT INTO mysql_query_rules (rule_id,active,match_digest,error_msg,apply) VALUES (1000,1,'^[(select)|(insert)|(update)|(delete)|(create)|(drop)|(reload)|(process)|(references)|(index)|(alter)|(show databases)|(create temporary tables)|(lock tables)|(execute)|(replication slave)|(replication client)|(create view)|(show view)|(create routine)|(alter routine)|(create user)|(event)|(trigger)].* mysql\..*','Use of system database is forbidden !',1);"
+sudo mysql -uadmin -padmin -P 6032 -e "INSERT INTO mysql_query_rules (rule_id,active,schemaname,match_digest,error_msg,apply) VALUES (2000,1,'mysql','.','Use of system database is forbidden !',1);"
+sudo mysql -uadmin -padmin -P 6032 -e "LOAD MYSQL QUERY RULES TO RUN;"
+sudo mysql -uadmin -padmin -P 6032 -e "SAVE MYSQL QUERY RULES TO DISK;"
 
 #Setup cron task
-#echo "* * * * * /etc/proxysql_modified.sh" >> /etc/proxysqlcron
-#/usr/bin/crontab /etc/proxysqlcron
-
-touch /home/trove/.proxy.end
+echo "* * * * * /etc/proxysql_modified.sh" >> /etc/proxysqlcron
+/usr/bin/crontab /etc/proxysqlcron
