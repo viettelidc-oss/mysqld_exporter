aa258d8b207906929e6ff49d2fcbb4d872f90277
nguyenphituan <33312978+nguyenphituan@users.noreply.github.com>
2020-04-03 16:26:42 +0700

    5s resolution

--- proxysql_modified.sh
+++ proxysql_modified.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
-
 QUERY="select \"INSERT INTO mysql_users (username,password,default_hostgroup) VALUES (\", CONCAT(\"'\",User,\"'\"), \",\", CONCAT(\"'\",Password,\"'\"), \",0);\" from mysql.user WHERE Password LIKE \"*%\" order by User;"
-mysql -uadmin -padmin -P 6032 -e "delete from mysql_users;"
-mysql -B -s -P 9999 -e "$QUERY" | while read line ; do mysql -uadmin -padmin -P 6032 -e "$line"; done
-mysql -uadmin -padmin -P 6032 -e "LOAD MYSQL USERS TO RUNTIME;"
-#mysql -P 6032 -e "SAVE MYSQL USERS TO DISK" -- No need to save to disk since we sync every 5s
+
+PROXY="select username,password from mysql_users WHERE password LIKE \"*%\" order by username;"
+MYSQL="select user as username,password from mysql.user WHERE password LIKE \"*%\" order by user;"
+
+i=0
+
+while [ $i -lt 12 ]; do # 1 min cron job has 12 turns each has 5s sleep
+
+#compare proxysql user list and mysql user list:
+MYSQL_USER=`mysql -B -s -P 9999 -e "$MYSQL"`
+PROXY_USER=`mysql -B -s -uadmin -padmin -h 127.0.0.1 -P 6032 -e "$PROXY"` # without -h it'll called to -h localhost => to MySQL
+
+if [ "$MYSQL_USER" == "$PROXY_USER" ]
+then 
+sleep 5
+i=$(( i + 1 ))
+else
+mysql -uadmin -padmin -h 127.0.0.1 -P 6032 -e "delete from mysql_users;"
+mysql -B -s -P 9999 -e "$QUERY" | while read line ; do mysql -uadmin -padmin -h 127.0.0.1 -P 6032 -e "$line"; done
+mysql -uadmin -padmin -h 127.0.0.1 -P 6032 -e "LOAD MYSQL USERS TO RUNTIME;"
+sleep 5
+i=$(( i + 1 ))
+fi
+done
