b662f7bcd6c3691855108b2aa2a19e180b1214ef
viettelidc-oss <57147861+viettelidc-oss@users.noreply.github.com>
2020-08-04 14:01:25 +0700

    Update proxysql_modified.sh
    Add pass connect to DB. When create DB, script get pass from '/home/trove/.my.cnf' and login

--- proxysql_modified.sh
+++ proxysql_modified.sh
@@ -5,21 +5,24 @@ QUERY="select \"INSERT INTO mysql_users (username,password,default_hostgroup) VA
 PROXY="select username,password from mysql_users WHERE password LIKE \"*%\" order by username;"
 MYSQL="select user as username,password from mysql.user WHERE password LIKE \"*%\" order by user;"
 
+
+PASS=`cat /home/trove/.my.cnf | awk '/password/{print $3}'`
+
 i=0
 
 while [ $i -lt 12 ]; do # 1 min cron job has 12 turns each has 5s sleep
 
 #compare proxysql user list and mysql user list:
-MYSQL_USER=`mysql -B -s -P 9999 -uos_admin -pQ4dpVfj1s7ObjP81PzsBdnT1GSRIMurt6ab8 -h 127.0.0.1 -e "$MYSQL"`
+MYSQL_USER=`mysql -B -s -P 9999 -uos_admin -p$PASS -h 127.0.0.1 -e "$MYSQL"`
 PROXY_USER=`mysql -B -s -uadmin -padmin -h 127.0.0.1 -P 6032 -e "$PROXY"` # without -h it'll called to -h localhost => to MySQL
 
 if [ "$MYSQL_USER" == "$PROXY_USER" ]
-then
+then 
 sleep 5
 i=$(( i + 1 ))
 else
 mysql -uadmin -padmin -h 127.0.0.1 -P 6032 -e "delete from mysql_users;"
-mysql -B -s -uos_admin -pQ4dpVfj1s7ObjP81PzsBdnT1GSRIMurt6ab8 -h 127.0.0.1 -P 9999 -e "$QUERY" | while read line ; do mysql -uadmin -padmin -h 127.0.0.1 -P 6032 -e "$line"; done
+mysql -B -s -uos_admin -p$PASS -h 127.0.0.1 -P 9999 -e "$QUERY" | while read line ; do mysql -uadmin -padmin -h 127.0.0.1 -P 6032 -e "$line"; done
 mysql -uadmin -padmin -h 127.0.0.1 -P 6032 -e "LOAD MYSQL USERS TO RUNTIME;"
 sleep 5
 i=$(( i + 1 ))
